c51c581a154c05b12c10a1fb28233f06cb9648f6
Daria <petrenko.dariaaa@gmail.com>
2018-10-04 12:37:03 +0300

    пробная версия форматирования номера

--- scripts/form.js
+++ scripts/form.js
@@ -1,45 +1,55 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $('input#phone').unbind().blur( function(){
+//     $('input#phone').unbind().blur( function(){
         
-        var val = $(this).val();
-        if (val.length > 15 && val != '')  
-        {  
-            $(this).addClass('not_error');
-            $(this).next('.error-box').text('')     
-        } else if ( val.length < 15 && val.length > 1) {
-            $(this).removeClass('not_error').addClass('error');
-            $(this).next('.error-box').html('&bull; полный номер телефона')
-                                        .css('color','red')
-                                        .animate({'paddingLeft':'10px'},400)
-                                        .animate({'paddingLeft':'5px'},400);
-        } else if (val.length == 0) {
-            $(this).removeClass('not_error').addClass('error');
-            $(this).next('.error-box').html('&bull; поле обязательно для заполнения')
-                                        .css('color','red')
-                                        .animate({'paddingLeft':'10px'},400)
-                                        .animate({'paddingLeft':'5px'},400);
-        }
+//         var val = $(this).val();
+//         if (val.length > 15 && val != '')  
+//         {  
+//             $(this).addClass('not_error');
+//             $(this).next('.error-box').text('')     
+//         } else if ( val.length < 15 && val.length > 1) {
+//             $(this).removeClass('not_error').addClass('error');
+//             $(this).next('.error-box').html('&bull; полный номер телефона')
+//                                         .css('color','red')
+//                                         .animate({'paddingLeft':'10px'},400)
+//                                         .animate({'paddingLeft':'5px'},400);
+//         } else if (val.length == 0) {
+//             $(this).removeClass('not_error').addClass('error');
+//             $(this).next('.error-box').html('&bull; поле обязательно для заполнения')
+//                                         .css('color','red')
+//                                         .animate({'paddingLeft':'10px'},400)
+//                                         .animate({'paddingLeft':'5px'},400);
+//         }
+//     });
+
+//     $("#ajaxform").submit(function(e){ 
+
+        
+//         e.preventDefault();
+        
+//         if ($('.not_error').length == 1) {  
+//             $('input#phone').removeClass('error').next('.error-box').text('')
+//                                                     .css('border-color','green');
+//             $('input#phone').next('.error-box').text('');
+//             alert('all are ok');
+//         } else {
+//             $('input#phone').removeClass('not_error').addClass('error');
+//             $('input#phone').next('.error-box').html('&bull; обязательное поле ввода')
+//                                         .css('color','red')
+//                                         .animate({'paddingLeft':'10px'},400)
+//                                         .animate({'paddingLeft':'5px'},400);
+//             }
+//     }); 
+
+
+//  }); // end script
+
+function phoneFormatter() {
+    $('.phone').on('input', function() {
+      var number = $(this).val().replace(/[^\d]/g, '')
+        number = number.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "($1) $2-$3-$4");
+      $(this).val(number)
     });
-
-    $("#ajaxform").submit(function(e){ 
-
-        
-        e.preventDefault();
-        
-        if ($('.not_error').length == 1) {  
-            $('input#phone').removeClass('error').next('.error-box').text('')
-                                                    .css('border-color','green');
-            $('input#phone').next('.error-box').text('');
-            alert('all are ok');
-        } else {
-            $('input#phone').removeClass('not_error').addClass('error');
-            $('input#phone').next('.error-box').html('&bull; обязательное поле ввода')
-                                        .css('color','red')
-                                        .animate({'paddingLeft':'10px'},400)
-                                        .animate({'paddingLeft':'5px'},400);
-            }
-    }); 
-
-
- }); // end script
+  };
+  
+  $(phoneFormatter);
