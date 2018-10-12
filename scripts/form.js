d6c6b48b21e021a33b4df64c44d0e5a682054977
Michael Kalygin <michael.kalygin@gmail.com>
2018-10-13 00:51:55 +0300

    Keep caret position on edit

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,6 @@
 // вход: значение input.val()
 // выход: отформатированное значение input.val()
-// алгоритм: 
+// алгоритм:
 // 1) Взять только цифры;
 // 2) Отформатировать цифры по маске;
 // 3) Обрезать лишние символы после последней введённой цифры.
@@ -12,7 +12,7 @@
 $(document).ready(function(){
 
     var FORMAT_MASK = "($1) $2-$3-$4";
-    var FORMAT_REGEXP = /^(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})$/; 
+    var FORMAT_REGEXP = /^(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})$/;
 
     function getNumbers (inputNumbers) {
 
@@ -30,7 +30,7 @@ $(document).ready(function(){
         var regexp = truncateRegexp(numbersCount);
         var resultPhone = formattedString.replace(regexp, "$1");
         return resultPhone;
-    } 
+    }
 
     function truncateRegexp (numbersCount) {
         var partRegexp1 = '^((\\D*\\d){';
@@ -39,17 +39,45 @@ $(document).ready(function(){
         return regExp;
     }
 
-    $('.phone').on('input',function(){
+    function getInputMetadata(input) {
+        var $input = $(input);
+        var inputValue = $input.val();
+        var rawValue = getNumbers(inputValue);
+        var start = input.selectionStart;
+        var leftPart = inputValue.slice(0, start);
+        var position = getNumbers(leftPart).length;
+
+        return {
+            rawValue: rawValue,
+            position: position,
+        };
+    }
+
+    function getCaretPosition(phoneNumber, numberPosition) {
+        var numbersCount = 0;
+
+        for (var i = 0; i <= phoneNumber.length; ++i) {
+            if (numberPosition === numbersCount) return i;
+            numbersCount += /\d/.test(phoneNumber[i]) ? 1 : 0;
+        }
+    }
+
+    $('.phone').on('input', function () {
         var $input = $(this);
         var inputNumbers = $input.val();
+        var metadata = getInputMetadata(this);
 
         var numbers = getNumbers(inputNumbers);
         var numbersCount = numbers.length;
-        var formattedNumbers = applyMask(numbers);    
+        var formattedNumbers = applyMask(numbers);
         var phoneNumber = truncateResult(formattedNumbers, numbersCount);
 
         $input.val(phoneNumber);
 
+        var numberPosition = metadata.position;
+        var caretPosition = getCaretPosition(phoneNumber, numberPosition);
+
+        this.setSelectionRange(caretPosition, caretPosition);
     });
 
     console.log(getNumbers("9854-55_frs") === "985455");
@@ -67,7 +95,7 @@ $(document).ready(function(){
     console.log(truncateResult("(845) 878-34-", 8) === "(845) 878-34");
     console.log(truncateResult("(84) --", 2) === "(84");
     console.log(truncateResult("(845) 108-70-90", 10) === "(845) 108-70-90");
-    console.log(truncateResult("() --", 0 ) === ""); 
+    console.log(truncateResult("() --", 0 ) === "");
     console.log(truncateResult("(845) 878-3-", 7) === "(845) 878-3");
     console.log(truncateResult("(8) --", 1 ) === "(8");
     console.log(truncateResult("(845) 1--", 4) === "(845) 1");
